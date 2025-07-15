4d48df1c75bb510db7790ff2f1a67b4f848181e2
Fkinghell <rajbeersharma28@gmail.com>
2025-07-15 23:04:12 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/edencart_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/edencart_app/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.example.edencart_app
+package com.example.edencart_app;
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 
-class MainActivity: FlutterActivity() {
+public class MainActivity extends FlutterActivity {
 }
